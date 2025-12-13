90af64e0f208998789bb4b5dc760a99422b41a92
spearscjs <spearcjs@gmail.com>
2025-12-12 19:21:30 -0500

    dbt amount col added + cents_to_dollars macro

--- snowflake_dbt/models/example/my_first_dbt_model.sql
+++ snowflake_dbt/models/example/my_first_dbt_model.sql
@@ -11,9 +11,9 @@
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, 314 as amount
     union all
-    select null as id
+    select 2 as id, 318 as amount
 
 )
 
--- snowflake_dbt/models/example/my_second_dbt_model.sql
+++ snowflake_dbt/models/example/my_second_dbt_model.sql
@@ -1,6 +1,5 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+select id, {{cents_to_dollars('amount', 2)}} as amount
 from {{ ref('my_first_dbt_model') }}
-where id = 1
